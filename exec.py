1e9723f150793ec8e3b41533e441ed3df2fcf577
akselele <axelelexd@gmail.com>
2020-04-24 21:00:57 +0200

    working on generating a json file with all emotes from betterttv

---
+++ exec.py
@@ -0,0 +1,24 @@
+# import json
+# from selenium import webdriver
+
+
+print('test')
+
+# jsondict = {{}}
+    
+# self.driver = webdriver.Chrome()
+
+    
+# self.driver.get('https://betterttv.com/emotes/top')
+
+
+
+# # maindiv = self.driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/div[2]/div/div[2]/div[1]/div')
+# # for i in maindiv:
+# i = 1
+# src = self.driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/div[2]/div/div[2]/div[1]/div/a['i']/img').get_attribute("src")
+# name = self.driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/div[2]/div/div[2]/div[1]/div/a['i']/div[1]').get_attribute("value")
+# jsondict[i].update({name:src})
+
+# print('test')
+# print(jsondict)
